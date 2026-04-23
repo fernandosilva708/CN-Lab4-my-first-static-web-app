cd7a6a7d3f7e493ad03db5b911fd3c26d905e0f9
Fernando Silva <fmsilva.mail@proton.me>
2026-04-23 11:23:06 +0100

    ficheiro de nodejs

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Olá, Laboratorio 4 !');
+  res.send('Olá, Laboratorio 4 node js  !');
 });
 
 app.listen(port, () => {
   console.log(`Servidor em http://localhost:${port}`);
 });
+
+
